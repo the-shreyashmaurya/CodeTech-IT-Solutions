9f4e5493a10b8dc408c2be4ae632c9fce23038b2
the-shreyashmaurya <shreyashmaurya3112@gmail.com>
2024-01-31 20:47:20 +0530

    Updated

--- university_attendance_app/lib/widgets/subjects_card.dart
+++ university_attendance_app/lib/widgets/subjects_card.dart
@@ -3,13 +3,17 @@ import 'package:university_attendance_app/constants/textstyle.dart';
 import 'package:university_attendance_app/database/attendanceRecords/attendanceRecords_database.dart';
 import 'package:university_attendance_app/database/courses/courses_database.dart';
 import 'package:university_attendance_app/database/courses/courses_model.dart';
-import 'package:university_attendance_app/screens/course_screen/course_screen.dart';
+// import 'package:university_attendance_app/database/courses/courses_database.dart';
+// import 'package:university_attendance_app/database/courses/courses_model.dart';
+// import 'package:university_attendance_app/screens/course_screen/course_screen.dart';
 
 class SubjectsCard extends StatefulWidget {
   final String courseId;
   final String studentId;
   final String date;
-  const SubjectsCard({
+  bool isLoading = true;
+  Map<String, dynamic> courseDetails = {};
+  SubjectsCard({
     super.key,
     required this.courseId,
     required this.studentId,
@@ -21,73 +25,94 @@ class SubjectsCard extends StatefulWidget {
 }
 
 class _SubjectsCardState extends State<SubjectsCard> {
-  Map<String, dynamic> courseDetails = {};
+  getCourseDetails() async {
+    CoursesModel courseDetailsModel =
+        await CoursesDatabase().getCourseDetails(courseId: widget.courseId);
+    var course = courseDetailsModel.toJson();
+    setState(() {
+      widget.courseDetails = course;
+      widget.isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getCourseDetails();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        // Update Attendance
-        await AttendanceRecordsDatabase()
-            .updateAttendanceForCourse(widget.studentId, widget.courseId);
-      },
-      child: Container(
-        margin: EdgeInsets.only(right: 2, left: 2),
-        width: 175,
-        child: Card(
-          color: Colors.white,
-          surfaceTintColor: Colors.white,
-          elevation: 4,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-              side: BorderSide(
-                color: Colors.red,
-                width: 2,
-              )),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color.fromRGBO(187, 141, 230, 1),
-                        Color.fromRGBO(128, 75, 179, 1),
-                      ],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(20)),
-                  ),
-                ),
-              ),
-              Expanded(
+    if (widget.isLoading == true) {
+      return Container(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return GestureDetector(
+        onTap: () async {
+          // Update Attendance
+          await AttendanceRecordsDatabase()
+              .updateAttendanceForCourse(widget.studentId, widget.courseId);
+        },
+        child: Container(
+          margin: EdgeInsets.only(right: 2, left: 2),
+          width: 175,
+          child: Card(
+            color: Colors.white,
+            surfaceTintColor: Colors.white,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(
+                  color: Colors.red,
+                  width: 2,
+                )),
+            child: Column(
+              children: [
+                Expanded(
                   child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(20),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(187, 141, 230, 1),
+                          Color.fromRGBO(128, 75, 179, 1),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20)),
+                    ),
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      courseDetails["name"],
-                      style: impNormalTextStyle,
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(20),
                     ),
-                    Text(
-                      courseDetails["instructor"],
-                      style: normalTextStyle,
-                    ),
-                  ],
-                ),
-              ))
-            ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.courseDetails["name"],
+                        style: impNormalTextStyle,
+                      ),
+                      Text(
+                        widget.courseDetails["instructor"],
+                        style: normalTextStyle,
+                      ),
+                    ],
+                  ),
+                ))
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
