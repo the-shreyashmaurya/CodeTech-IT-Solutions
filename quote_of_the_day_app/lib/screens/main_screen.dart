aa6f2dbc36a9d514629a3e157c66460cf8aad024
the-shreyashmaurya <shreyashmaurya3112@gmail.com>
2024-02-09 22:03:54 +0530

    Updated

---
+++ quote_of_the_day_app/lib/screens/main_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        // gradient: LinearGradient(colors: [Colors.black, Colors.red]),
+        image: DecorationImage(
+          image: AssetImage('images/background.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
